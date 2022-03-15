19e4bac989ac3ad4cd9bf494f27c50909bfbb8bd
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2022-03-15 16:15:37 -0300

    Finalizando o desafio em POO com Java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -26,9 +28,27 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
 
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java developer");
+        bootcamp.setDescricao("Java do básico ao avançado");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRamonPessoa = new Dev();
+        devRamonPessoa.setNome("Ramon Palheta Pessôa de Souza");
+        devRamonPessoa.inscreverBootcamp(bootcamp);
+        System.out.println("COnteudo(os) inscrito(os) "+ devRamonPessoa.getConteudosInscritos());
+        devRamonPessoa.progredir();
+
+        System.out.println("COnteudo(os) inscrito(os) "+ devRamonPessoa.getConteudosInscritos());
+        System.out.println("COnteudo(os) concluído(os) "+ devRamonPessoa.getConteudoConcluidos());
+        System.out.println("XP:" + devRamonPessoa.calcularTotalXp());
     }
 }
